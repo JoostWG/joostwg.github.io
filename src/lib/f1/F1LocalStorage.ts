715423be5dbdaf3169e0670b59188499e0f3aedf
JoostWG <122867812+JoostWG@users.noreply.github.com>
2026-01-20 23:40:16 +0100

    💚 FIx build error

--- src/lib/f1/F1LocalStorage.ts
+++ src/lib/f1/F1LocalStorage.ts
@@ -1,45 +1,47 @@
-import type { ApiCache, Pagination } from 'jolpica-f1-api'
+// f1-garage does not have cache support yet. I believe the data is cached by browser anyway
 
-export class F1LocalStorage implements ApiCache {
-  public async get<T>(path: string, pagination?: Pagination): Promise<T | null> {
-    const cached = localStorage.getItem(this.getCacheKey(path, pagination))
+// import type { ApiCache, Pagination } from 'jolpica-f1-api'
 
-    if (!cached) {
-      return null
-    }
+// export class F1LocalStorage implements ApiCache {
+//   public async get<T>(path: string, pagination?: Pagination): Promise<T | null> {
+//     const cached = localStorage.getItem(this.getCacheKey(path, pagination))
 
-    const { timestamp, data } = JSON.parse(cached) as { timestamp: number; data: T }
+//     if (!cached) {
+//       return null
+//     }
 
-    // TODO: Cache responses that will never change forever... if possible
-    // Day
-    if (Date.now() - timestamp >= 86400) {
-      return null
-    }
+//     const { timestamp, data } = JSON.parse(cached) as { timestamp: number; data: T }
 
-    return data
-  }
+//     // TODO: Cache responses that will never change forever... if possible
+//     // Day
+//     if (Date.now() - timestamp >= 86400) {
+//       return null
+//     }
 
-  public async set(
-    data: unknown,
-    cacheControl: unknown | undefined,
-    path: string,
-    pagination?: Pagination,
-  ): Promise<void> {
-    console.log(333)
-    localStorage.setItem(
-      this.getCacheKey(path, pagination),
-      JSON.stringify({ timestamp: Date.now(), data }),
-    )
-  }
+//     return data
+//   }
 
-  private getCacheKey(path: string, pagination?: Pagination): string {
-    return (
-      'f1cache.' +
-      path
-        .split('/')
-        .filter((part) => part)
-        .concat([String(pagination?.limit ?? 'null'), String(pagination?.offset ?? 'null')])
-        .join('-')
-    )
-  }
-}
+//   public async set(
+//     data: unknown,
+//     cacheControl: unknown | undefined,
+//     path: string,
+//     pagination?: Pagination,
+//   ): Promise<void> {
+//     console.log(333)
+//     localStorage.setItem(
+//       this.getCacheKey(path, pagination),
+//       JSON.stringify({ timestamp: Date.now(), data }),
+//     )
+//   }
+
+//   private getCacheKey(path: string, pagination?: Pagination): string {
+//     return (
+//       'f1cache.' +
+//       path
+//         .split('/')
+//         .filter((part) => part)
+//         .concat([String(pagination?.limit ?? 'null'), String(pagination?.offset ?? 'null')])
+//         .join('-')
+//     )
+//   }
+// }
